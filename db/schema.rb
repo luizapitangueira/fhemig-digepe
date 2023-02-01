2d7abf648dcb3868c60070d40d15f6081fb0c3b7
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-01-31 21:07:21 -0300

    Update user name field

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2023_01_24_150119) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "nome"
+    t.string "name"
     t.boolean "admin"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,4 @@ print "Deletando todos os usuários"
 User.delete_all
 # Criar usuário
 print("Criando todos os usuários")
-User.create nome:'Gabriel Dornas', admin:true, email: 'gabrielbdornas@gmail.com', password: 123456, password_confirmation:123456
+User.create name:'Gabriel Dornas', admin:true, email: 'gabrielbdornas@gmail.com', password: 123456, password_confirmation:123456
