b6e3c41b5a6868593f9dc597ebb13b13b8d7abeb
Jemisson <jemison321@gmail.com>
2023-07-18 15:09:15 -0400

    Alterações em config/application.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
-  config.eager_load = true
+  config.eager_load = false
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
@@ -26,12 +26,15 @@ Rails.application.configure do
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
 
+    # Compress JavaScripts and CSS.
+    config.assets.js_compressor = Uglifier.new(harmony: true)
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress CSS using a preprocessor.
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
