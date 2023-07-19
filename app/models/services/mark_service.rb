28a678cd2dc073f987493d9c4170ea3c2d25611d
Jemisson <jemison321@gmail.com>
2023-07-19 10:47:59 -0400

    Comentando código não utilizado

--- app/models/services/mark_service.rb
+++ app/models/services/mark_service.rb
@@ -3,12 +3,19 @@
 class MarkService
 	class << self
 		def last_mark(count_jobs_all,count_workload_all)
+			# {
+			# 	jobs: Mark.last.n_jobs,
+			# 	workload: Mark.last.n_workload,
+			# 	percent_jobs: ((count_jobs_all.to_f / Mark.last.n_jobs.to_f ) * 100).round(2),
+			# 	percent_workload: ((count_workload_all.to_f / Mark.last.n_workload.to_f) * 100).round(2),
+			# 	publish_date: Mark.last.publication_date
+			# }
 			{
-				jobs: Mark.last.n_jobs,
-				workload: Mark.last.n_workload,
-				percent_jobs: ((count_jobs_all.to_f / Mark.last.n_jobs.to_f ) * 100).round(2),
-				percent_workload: ((count_workload_all.to_f / Mark.last.n_workload.to_f) * 100).round(2),
-				publish_date: Mark.last.publication_date
+				jobs: 0,
+				workload: 0,
+				percent_jobs: 0,
+				percent_workload: 0,
+				publish_date: 0
 			}
 		end
 	end
