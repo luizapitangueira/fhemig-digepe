c69ad6429c32da8377ed6d0c81587cf1b85a0047
Luiza Santana Pitangueira <luiza.pitangueira@gmail.com>
2023-08-04 11:15:03 -0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-require Rails.root.join('db/seeds/careers_populate.rb')
-require Rails.root.join('db/seeds/hospitals_populate.rb')
-require Rails.root.join('db/seeds/employees_jobs_populate.rb')
+#require Rails.root.join('db/seeds/careers_populate.rb')
+#require Rails.root.join('db/seeds/hospitals_populate.rb')
+#require Rails.root.join('db/seeds/employees_jobs_populate.rb')
 
 print "Criando Usuários "
 User.find_or_create_by(email: 'admin@example.com') do |u|
@@ -10,14 +10,14 @@ User.find_or_create_by(email: 'admin@example.com') do |u|
 end                  
 puts "-- OK!"
                            
-print "Criando Carreiras "
-CareersPopulate.populate!
-puts "-- OK!"
+#print "Criando Carreiras "
+#CareersPopulate.populate!
+#puts "-- OK!"
 
-print "Criando Hospitais "
-HospitalsPopulate.populate!
-puts "-- OK!"
+#print "Criando Hospitais "
+#HospitalsPopulate.populate!
+#puts "-- OK!"
 
-print "Criando Vagas e Contratados  "
-EmployeesJobsPopulate.populate!
-puts "-- OK!"
+#print "Criando Vagas e Contratados  "
+#EmployeesJobsPopulate.populate!
+#puts "-- OK!"
